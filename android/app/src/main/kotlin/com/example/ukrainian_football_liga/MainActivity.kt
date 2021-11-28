c7e7a3ee56bc454cfe136de44a4e926c9ca2a806
Nazarko12 <nazartsiupryk@gmail.com>
2021-11-28 18:48:48 +0200

    Added files representing the work on the design of football themes

---
+++ android/app/src/main/kotlin/com/example/ukrainian_football_liga/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ukrainian_football_liga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/Product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String? title, image;
+
+  Product({this.title, this.image});
+}
+
+List<Product> demo_products = [
+  Product(title: "FC Shaktar Donetsk", image: "assets/images/shakhtar.png"),
+  Product(title: "FC Dynamo Kyiv", image: "assets/images/dynamo.jpg"),
+  Product(title: "FC Vorskla Poltava", image: "assets/images/vorskla.png"),
+  Product(title: "FC Zorya Luhansk", image: "assets/images/zorya.jpeg"),
+  Product(title: "FC Oleksandria", image: "assets/images/oleksandria.png"),
+];
