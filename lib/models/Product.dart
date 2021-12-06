75b0f8b4010273047e69ebaa13ee9ea4ad0dad46
Nazarii <57871748+nazartsiupryk@users.noreply.github.com>
2021-12-06 16:21:59 +0200

    Update Product.dart

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -7,7 +7,7 @@ class Product {
 List<Product> demo_products = [
   Product(title: "FC Shaktar Donetsk", image: "assets/images/shakhtar.png"),
   Product(title: "FC Dynamo Kyiv", image: "assets/images/dynamo.jpg"),
-  Product(title: "FC Vorskla Poltava", image: "assets/images/vorskla.png"),
+  Product(title: "FC Vorskla Poltava", image: "assets/images/vorskla.jpg"),
   Product(title: "FC Zorya Luhansk", image: "assets/images/zorya.jpeg"),
   Product(title: "FC Oleksandria", image: "assets/images/oleksandria.png"),
 ];
